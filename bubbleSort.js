2b441b427125464f861d159f2bd0965df21dda28
kostadinov1 <evgenikostadinov1987@gmail.com>
2023-09-28 20:49:51 +0300

    added bubble sort

---
+++ bubbleSort.js
@@ -0,0 +1,18 @@
+
+
+
+export function bubbleSort(arr) {
+    // loop over the whole array
+    for (let i = 0; i < arr.length; i++) {
+        // loop over the array but one element ahead
+        for (let j = 0; j < arr.length - i - 1; j++) {
+            // if left element is greater then the right element => Swap them
+            if (arr[j] > arr[j + 1]) {
+                let temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j +1] = temp;
+            }
+        }
+    }
+    return arr;
+}
